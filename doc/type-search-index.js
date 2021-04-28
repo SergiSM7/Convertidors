007bdd15fb09cbcc1195af075bff74eedc94a5e5
SergiSM7 <65180552+SergiSM7@users.noreply.github.com>
2021-04-28 21:39:23 +0200

    Commit Temps documentada

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Temperatura"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Temps"}]
